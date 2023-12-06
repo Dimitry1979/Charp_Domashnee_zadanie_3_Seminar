4b46a4c134c9d3b9575af7e9880c23dd396d8541
Dmitry <semiashkin@tut.by>
2023-12-06 21:32:10 +0300

    Решили задачу

--- Zadacha_1/Program.cs
+++ Zadacha_1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100.
+// Найдите количество элементов массива, значения которых лежат в отрезке
+// [20,90].
+
+
+int N=10;
+int[] a=new int[N];
+Random random=new Random();
+for(int i=0;i<a.Length;i++)
+a[i]=random.Next(1,100);
+for(int i=0;i<a.Length;i++)
+System.Console.Write($"{a[i],4}");
+
+int count=0;
+for(int i=0; i<a.Length;i++)
+{
+if(a[i]>20 && a[i]<90)
+count++;
+}
+System.Console.WriteLine();
+System.Console.WriteLine($"{count} Элементов");
